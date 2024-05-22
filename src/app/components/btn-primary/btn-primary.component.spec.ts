cb76659de92d926c05c98c752f4a2eabcec14c8c
VitorOliveiraEWGF <135755701+VitorOliveiraEWGF@users.noreply.github.com>
2024-05-22 16:33:44 -0300

    commit inicial

--- src/app/components/btn-primary/btn-primary.component.spec.ts
+++ src/app/components/btn-primary/btn-primary.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BrnPrimaryComponent } from './brn-primary.component';
+import { BtnPrimaryComponent } from './btn-primary.component';
 
 describe('BrnPrimaryComponent', () => {
-  let component: BrnPrimaryComponent;
-  let fixture: ComponentFixture<BrnPrimaryComponent>;
+  let component: BtnPrimaryComponent;
+  let fixture: ComponentFixture<BtnPrimaryComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BrnPrimaryComponent]
+      imports: [BtnPrimaryComponent]
     })
     .compileComponents();
-    
-    fixture = TestBed.createComponent(BrnPrimaryComponent);
+
+    fixture = TestBed.createComponent(BtnPrimaryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
